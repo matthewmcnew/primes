5f88a946aec5f27ee53d1d920d4f200a500a5930
Matthew McNew <matthewmcnew@gmail.com>
2014-05-24 15:19:04 -0600

    Added simple main

--- main.go
+++ main.go
@@ -1,3 +1,31 @@
 package main
 
-func main() {}
+import (
+	"github.com/matthewmcnew/primes/pool"
+
+	"flag"
+	"strconv"
+	"fmt"
+)
+
+func main() {
+	flag.Parse()
+
+	numCPUS, err := strconv.Atoi(flag.Arg(0))
+	if err != nil {
+		fmt.Println("Please Provide Valid # of Num Cpus")
+		return
+	}
+
+	maximumNumberToComputeTo, err := strconv.Atoi(flag.Arg(1))
+	if err != nil {
+		fmt.Println("Please Provide Valid maximum number to convert to")
+		return
+	}
+
+	pool := pool.NewPool(numCPUS)
+
+	result := pool.Run(maximumNumberToComputeTo)
+
+	fmt.Println("Most Common:", result)
+}
